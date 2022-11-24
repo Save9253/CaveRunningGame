fa72db81d249f6ddc7d2c4f0e143f8c5fbd7015a
Save <66020327+Save9253@users.noreply.github.com>
2022-11-24 17:15:53 -0500

    added a couple more functions

--- Hold.vhd
+++ Hold.vhd
@@ -7,37 +7,38 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
-ENTITY VariableCounter IS
+ENTITY Hold IS
 
 PORT(
     clk,enable,resetn : IN STD_LOGIC;
-    countTo : in integer range 0 to 7;
-    done   : out std_logic
+    holdFor : in integer range 0 to 7;
+    output   : out std_logic
 );
-END VariableCounter;
+END Hold;
 
-ARCHITECTURE Counter OF VariableCounter IS
+ARCHITECTURE Holder OF Hold IS
     signal count  : integer range 0 to 7;
 BEGIN
     PROCESS (clk,resetn,enable)
     BEGIN
         If (resetn = '0') then
             count <= 0;
-            done <= '0';
+            output <= '0';
         ELSIF (clk'EVENT AND clk = '1') THEN
-            
-            done <= '0';
+            if (holdFor = 0) then
+                    output <= '0';
+            end if;
 
             if (enable = '1') then
-                if (countTo = 0) then
-                    done <= '1';
+                if (holdFor = 0) then
+                    output <= '1';
                 else
-                    if (count = countTo) then
-                        done <= '1';
+                    if (count = holdFor) then
                         count <= 0;
+                        output <= '0';
                     else 
                         count <= count + 1;
-                        done <= '0';
+                        output <= '1';
                     end if;
                 end if;
             end if;
@@ -45,4 +46,4 @@ BEGIN
         END IF;
     END PROCESS;
 
-END Counter;
+END Holder;
