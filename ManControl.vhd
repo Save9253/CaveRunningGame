40775bdbd4356563174f6c25b05a43ebe4c19330
CharlieBird21118 <90112813+CharlieBird21118@users.noreply.github.com>
2022-12-07 11:24:37 -0500

    deleted things

--- ManControl.vhd
+++ ManControl.vhd
@@ -12,7 +12,6 @@ ENTITY ManControl IS
 
 PORT(
     clk,resetn,enable,jump,duck : IN STD_LOGIC;
-    manPositionT,manPositionB : buffer std_logic:='0';
     manHex : out std_logic_vector (6 downto 0) := "1001111"
 );
 END ManControl;
@@ -26,23 +25,15 @@ BEGIN
     PROCESS (clk,resetn,enable)
     BEGIN
         If (resetn = '0') then
-            manPositionT <= '0';
-            manPositionB <= '0';
             manHex <= "1001111";
         ELSIF (clk'EVENT AND clk = '1') THEN
             if (jump = '1') then
-                manPositionT <= '0';
-                manPositionB <= '1';
 					 manHEX <= jumpHex;
             elsif (duck = '1') then
-                manPositionT <= '1';
-                manPositionB <= '0';
 					 manHEX <= duckHEx;
             end if;
 
             if (enable = '1') then
-                manPositionB <= '0';
-                manPositionT <= '0';
 					manHEX <= run_step1;
 				END IF;
 			END IF;
